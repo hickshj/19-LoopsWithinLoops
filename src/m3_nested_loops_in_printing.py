92422272ffac54f6070156daac72ece0ee9d2f14
hickshj <hickshj@rose-hulman.edu>
2018-10-17 10:42:04 -0400

    Saint James Infirmary

--- src/m3_nested_loops_in_printing.py
+++ src/m3_nested_loops_in_printing.py
@@ -301,7 +301,7 @@ def triangle_all_numbers_in_each_row(r):
     Precondition:  r is a non-negative integer.
     """
     # ------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # Done: 7. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -309,6 +309,10 @@ def triangle_all_numbers_in_each_row(r):
     #   in this or the other problems in this module, as doing so
     #   would defeat the goal of providing practice at loops within loops.
     # ------------------------------------------------------------------
+    for k in range(r):
+        for i in range(k + 1):
+            print(i + 1, end="")
+        print()
 
 
 # ----------------------------------------------------------------------
